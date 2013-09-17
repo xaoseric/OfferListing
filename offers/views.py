e07464172a0a35c3d4d25e04e815dff176fb0d18
Darayus Nanavati <darayus.contact@gmail.com>
2013-09-17 13:43:47 +1000

    Added saving to the form

--- offers/views.py
+++ offers/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
-from offers.models import Offer
+from offers.models import Offer, Comment
 from offers.forms import CommentForm
+from django.contrib import messages
 
 
 def view_offer(request, offer_pk):
@@ -10,6 +11,20 @@ def view_offer(request, offer_pk):
 
     if request.method == "POST":
         form = CommentForm(request.POST)
+        if form.is_valid():
+            if request.user.is_authenticated():
+                Comment(
+                    commenter=request.user,
+                    offer=offer,
+                    content=form.cleaned_data["comment"],
+                    status=Comment.PUBLISHED
+                ).save()
+                messages.success(request, "Thank you for commenting!")
+                form = CommentForm()
+            else:
+                messages.error(request, 'You need to be logged in to comment!')
+        else:
+            messages.error(request, "Your comment had errors. Please fix them and submit again!")
     else:
         form = CommentForm()
 
