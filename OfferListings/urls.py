bd50ce7d23c5b8475c3f2e7c9eb0ebea62c87b0d
Darayus Nanavati <darayus.contact@gmail.com>
2013-09-17 15:54:45 +1000

    Fixed all pagination

--- OfferListings/urls.py
+++ OfferListings/urls.py
@@ -4,7 +4,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^((?P<page_number>\d+)/)?$', 'offers.views.list_offers', name='home'),
+    url(r'^$', 'offers.views.list_offers', name='home'),
+    url(r'^(?P<page_number>\d+)/$', 'offers.views.list_offers', name='home_pagination'),
     url(r'^offers/', include('offers.urls', namespace='offer')),
 
     url(r'^admin/', include(admin.site.urls)),
