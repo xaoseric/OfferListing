3498855ba676e2877e6aad7c09dbf768d6ec6d83
Darayus Nanavati <darayus.contact@gmail.com>
2013-09-17 11:21:09 +1000

    Added tests for offer types

--- offers/tests.py
+++ offers/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from offers.models import Offer, Provider, Plan
 from model_mommy import mommy
 from django.utils.text import slugify
+from django.core.urlresolvers import reverse
 
 
 class OfferMethodTests(TestCase):
@@ -24,6 +25,44 @@ class OfferMethodTests(TestCase):
         for offer in self.offers:
             name = "{0} ({1})".format(offer.name, offer.provider.name)
             self.assertEqual(name, offer.__unicode__())
+
+
+class OfferViewTests(TestCase):
+    def setUp(self):
+        self.offers = mommy.make(Offer, _quantity=10)
+
+    def test_can_view_published_offers(self):
+        """
+        Test that you can view an offer that is published
+        """
+        for offer in self.offers:
+            offer.status = offer.PUBLISHED
+            offer.save()
+            response = self.client.get(reverse('offer:view', args=[offer.pk]))
+
+            self.assertEqual(response.status_code, 200)
+
+    def test_can_not_view_unpublished_offers(self):
+        """
+        Test that you can not view an offer that is not published
+        """
+        for offer in self.offers:
+            offer.status = offer.UNPUBLISHED
+            offer.save()
+            response = self.client.get(reverse('offer:view', args=[offer.pk]))
+
+            self.assertEqual(response.status_code, 404)
+
+    def test_can_not_view_draft_offers(self):
+        """
+        Test that you can not view an offer that is a draft
+        """
+        for offer in self.offers:
+            offer.status = offer.DRAFT
+            offer.save()
+            response = self.client.get(reverse('offer:view', args=[offer.pk]))
+
+            self.assertEqual(response.status_code, 404)
 
 
 class ProviderMethodTests(TestCase):
