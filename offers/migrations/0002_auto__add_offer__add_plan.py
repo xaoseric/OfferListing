cb4c8fc983fb4a6b2382f1f7b95bd4d5fca6ee06
Darayus Nanavati <darayus.contact@gmail.com>
2013-09-17 10:22:23 +1000

    Added offer and plan models

---
+++ offers/migrations/0002_auto__add_offer__add_plan.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Offer'
+        db.create_table(u'offers_offer', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('content', self.gf('django.db.models.fields.TextField')()),
+            ('provider', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['offers.Provider'])),
+            ('status', self.gf('django.db.models.fields.CharField')(default='d', max_length=1)),
+            ('created_at', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('updated_at', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
+        ))
+        db.send_create_signal(u'offers', ['Offer'])
+
+        # Adding model 'Plan'
+        db.create_table(u'offers_plan', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('offer', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['offers.Offer'])),
+        ))
+        db.send_create_signal(u'offers', ['Plan'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Offer'
+        db.delete_table(u'offers_offer')
+
+        # Deleting model 'Plan'
+        db.delete_table(u'offers_plan')
+
+
+    models = {
+        u'offers.offer': {
+            'Meta': {'object_name': 'Offer'},
+            'content': ('django.db.models.fields.TextField', [], {}),
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'provider': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['offers.Provider']"}),
+            'status': ('django.db.models.fields.CharField', [], {'default': "'d'", 'max_length': '1'}),
+            'updated_at': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
+        },
+        u'offers.plan': {
+            'Meta': {'object_name': 'Plan'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'offer': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['offers.Offer']"})
+        },
+        u'offers.provider': {
+            'Meta': {'object_name': 'Provider'},
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'start_date': ('django.db.models.fields.DateField', [], {}),
+            'updated_at': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['offers']
--- offers/models.py
+++ offers/models.py
@@ -8,3 +8,26 @@ class Provider(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+
+class Offer(models.Model):
+    PUBLISHED = 'p'
+    UNPUBLISHED = 'u'
+    DRAFT = 'd'
+
+    STATUS_CHOICES = (
+        (PUBLISHED, 'Published'),
+        (UNPUBLISHED, 'Unpublished'),
+        (DRAFT, 'Draft'),
+    )
+
+    name = models.CharField(max_length=255)
+    content = models.TextField()
+    provider = models.ForeignKey(Provider)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default=DRAFT)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Plan(models.Model):
+    offer = models.ForeignKey(Offer)
