c3495103784ecfd08bbcd85525037e540709c5a4
Darayus Nanavati <darayus.contact@gmail.com>
2013-09-17 15:56:12 +1000

    Prepend p to pagination

--- OfferListings/urls.py
+++ OfferListings/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'offers.views.list_offers', name='home'),
-    url(r'^(?P<page_number>\d+)/$', 'offers.views.list_offers', name='home_pagination'),
+    url(r'^p(?P<page_number>\d+)/$', 'offers.views.list_offers', name='home_pagination'),
     url(r'^offers/', include('offers.urls', namespace='offer')),
 
     url(r'^admin/', include(admin.site.urls)),
