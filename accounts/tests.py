5504055b8ab55fcdd1a00f2724883fc4a232bace
Darayus Nanavati <darayus.contact@gmail.com>
2013-09-19 22:21:37 +1000

    Added tests for editing a user account

--- accounts/tests.py
+++ accounts/tests.py
@@ -61,3 +61,51 @@ class ProfileViewTests(TestCase):
         self.assertEqual(len(response.redirect_chain), 1)
         self.assertTrue(reverse('login') in response.redirect_chain[0][0])
         self.assertEqual(response.redirect_chain[0][1], 302)
+
+
+class EditAccountViewTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user('some_user', 'test@example.com', 'password')
+        self.user.first_name = 'Joe'
+        self.user.last_name = 'Bill'
+        self.user.save()
+        self.client.login(username='some_user', password='password')
+
+    def test_can_view_edit_form(self):
+        """
+        Test that a user can view their own edit page
+        """
+        response = self.client.get(reverse('edit_account'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_logged_out_user_can_not_view_edit_form(self):
+        """
+        Test that a logged out user can not view the edit form of a profile.
+        """
+        self.client.logout()
+
+        response = self.client.get(reverse('profile'), follow=True)
+
+        self.assertEqual(200, response.status_code)
+
+        self.assertEqual(len(response.redirect_chain), 1)
+        self.assertTrue(reverse('login') in response.redirect_chain[0][0])
+        self.assertEqual(response.redirect_chain[0][1], 302)
+
+    def test_form_updates_user_correctly(self):
+        """
+        Test that posting the form data correctly updates the form
+        """
+        data = {
+            "first_name": "New_name",
+            "last_name": "Last_name",
+            "email": "test2@example.com",
+        }
+        response = self.client.post(reverse('edit_account'), data)
+
+        user = User.objects.get(pk=self.user.pk)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(user.first_name, data["first_name"])
+        self.assertEqual(user.last_name, data["last_name"])
+        self.assertEqual(user.email, data["email"])
