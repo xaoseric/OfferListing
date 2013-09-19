bdaf449f95f9e79a3de119600727a83255df09eb
Darayus Nanavati <darayus.contact@gmail.com>
2013-09-19 22:08:19 +1000

    Added tests for provider view

--- accounts/tests.py
+++ accounts/tests.py
@@ -27,3 +27,37 @@ class LogoutViewTests(TestCase):
         self.assertEqual(len(response.redirect_chain), 1)
         self.assertEqual(response.redirect_chain[0][0], 'http://testserver' + reverse('login'))
         self.assertEqual(response.redirect_chain[0][1], 302)
+
+
+class ProfileViewTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user('some_user', 'test@example.com', 'password')
+        self.user.first_name = 'Joe'
+        self.user.last_name = 'Bill'
+        self.user.save()
+        self.client.login(username='some_user', password='password')
+
+    def test_can_view_own_profile(self):
+        """
+        Test that the user can view their own profile when they are logged in
+        """
+        response = self.client.get(reverse('profile'))
+
+        self.assertEqual(200, response.status_code)
+        self.assertContains(response, 'Joe')
+        self.assertContains(response, 'Bill')
+        self.assertContains(response, 'some_user')
+
+    def test_can_not_view_profile_when_logged_out(self):
+        """
+        Test that the user can not view the profile page when logged out
+        """
+        self.client.logout()
+
+        response = self.client.get(reverse('profile'), follow=True)
+
+        self.assertEqual(200, response.status_code)
+
+        self.assertEqual(len(response.redirect_chain), 1)
+        self.assertTrue(reverse('login') in response.redirect_chain[0][0])
+        self.assertEqual(response.redirect_chain[0][1], 302)
