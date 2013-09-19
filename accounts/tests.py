6ae0ca7080e39b3021fe7c2c35869fdbf13c90ca
Darayus Nanavati <darayus.contact@gmail.com>
2013-09-19 12:30:45 +1000

    Added tests for the logout view

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,16 +1,29 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+class LogoutViewTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user('some_user', 'test@example.com', 'password')
+        self.client.login(username='some_user', password='password')
+
+    def test_logged_in_user_can_logout(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Tests that a logged in user can log out and is redirected to the login page
         """
-        self.assertEqual(1 + 1, 2)
+        response = self.client.get(reverse('logout'), follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.redirect_chain), 1)
+        self.assertEqual(response.redirect_chain[0][0], 'http://testserver' + reverse('login'))
+        self.assertEqual(response.redirect_chain[0][1], 302)
+
+    def test_logged_out_user_can_logout(self):
+        """
+        Test that a logged out user can still view the logout page and will be redirected to the login form
+        """
+        response = self.client.get(reverse('logout'), follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.redirect_chain), 1)
+        self.assertEqual(response.redirect_chain[0][0], 'http://testserver' + reverse('login'))
+        self.assertEqual(response.redirect_chain[0][1], 302)
