ededaca0127deba7800d1766cd71c4587c4c0ece
Darayus Nanavati <darayus.contact@gmail.com>
2013-09-19 22:36:00 +1000

    Added tests for the change password view

--- accounts/tests.py
+++ accounts/tests.py
@@ -84,7 +84,7 @@ class EditAccountViewTests(TestCase):
         """
         self.client.logout()
 
-        response = self.client.get(reverse('profile'), follow=True)
+        response = self.client.get(reverse('edit_account'), follow=True)
 
         self.assertEqual(200, response.status_code)
 
@@ -135,3 +135,95 @@ class EditAccountViewTests(TestCase):
         self.assertEqual(user.email, self.user.email)
 
         self.assertContains(response, 'The form had errors. Please correct them and submit again.')
+
+
+class ChangePasswordTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user('some_user', 'test@example.com', 'password')
+        self.user.first_name = 'Joe'
+        self.user.last_name = 'Bill'
+        self.user.save()
+        self.client.login(username='some_user', password='password')
+
+    def test_can_view_change_password_form(self):
+        """
+        Test that a user can view their own change password page
+        """
+        response = self.client.get(reverse('change_password'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_logged_out_user_can_not_view_change_password_form(self):
+        """
+        Test that a logged out user can not view the change password form
+        """
+        self.client.logout()
+
+        response = self.client.get(reverse('change_password'), follow=True)
+
+        self.assertEqual(200, response.status_code)
+
+        self.assertEqual(len(response.redirect_chain), 1)
+        self.assertTrue(reverse('login') in response.redirect_chain[0][0])
+        self.assertEqual(response.redirect_chain[0][1], 302)
+
+    def test_can_change_password(self):
+        """
+        Test that a user can change their password using the password
+        """
+
+        data = {
+            "new_password1": 'new_password',
+            "new_password2": "new_password",
+            "old_password": "password",
+        }
+        response = self.client.post(reverse('change_password'), data)
+
+        user = User.objects.get(pk=self.user.pk)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "You password has been changed!")
+        self.assertNotEqual(user.password, self.user.password)
+
+    def test_cant_change_password_with_incorrect_old_password(self):
+        """
+        Test that the user can not change their password if their old password is not correct
+        """
+        data = {
+            "new_password1": 'new_password',
+            "new_password2": "new_password",
+            "old_password": "wrong_password",
+        }
+        response = self.client.post(reverse('change_password'), data)
+
+        user = User.objects.get(pk=self.user.pk)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertFormError(
+            response,
+            'form',
+            'old_password',
+            'Your old password was entered incorrectly. Please enter it again.',
+        )
+        self.assertEqual(user.password, self.user.password)
+
+    def test_cant_change_password_with_incorrect_matching_passwords(self):
+        """
+        Test that the user can not change their password if the two new passwords provided did not match
+        """
+        data = {
+            "new_password1": 'new_password',
+            "new_password2": "new_password_wrong",
+            "old_password": "password",
+        }
+        response = self.client.post(reverse('change_password'), data)
+
+        user = User.objects.get(pk=self.user.pk)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertFormError(
+            response,
+            'form',
+            'new_password2',
+            'The two password fields didn\'t match.',
+        )
+        self.assertEqual(user.password, self.user.password)
