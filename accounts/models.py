e5727bacd868ad1918dd4515bd8e21d0c822e58a
Darayus Nanavati <darayus.contact@gmail.com>
2013-09-20 12:37:08 +1000

    Added unicode function

--- accounts/models.py
+++ accounts/models.py
@@ -7,6 +7,9 @@ class UserProfile(models.Model):
     birthday = models.DateField(blank=True, null=True)
     user = models.OneToOneField(User)
 
+    def __unicode__(self):
+        return "{0} profile".format(self.user.username)
+
 
 def create_user_profile(sender, instance, created, **kwargs):
     print sender, instance, created, kwargs
