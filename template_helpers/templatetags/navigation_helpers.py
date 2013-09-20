e213517ce5b71333a897fd3d73d3cd2df4678142
Darayus Nanavati <darayus.contact@gmail.com>
2013-09-21 00:35:14 +1000

    Added navigation_link helper

---
+++ template_helpers/templatetags/navigation_helpers.py
@@ -0,0 +1,14 @@
+from django import template
+from django.core.urlresolvers import reverse
+
+register = template.Library()
+
+
+@register.simple_tag
+def navigation_link(request, url, title, active_text='active'):
+    link = reverse(url)
+    active = ''
+    if request.path.startswith(link):
+        active = active_text
+
+    return "<li class='{0}'><a href='{1}'>{2}</a></li>".format(active, link, title)
