52176a2799e9f2b8c3b70045ede090def9541646
Darayus Nanavati <darayus.contact@gmail.com>
2013-09-20 13:52:20 +1000

    Added is_active field

--- offers/models.py
+++ offers/models.py
@@ -168,6 +168,7 @@ class Plan(models.Model):
     url = models.TextField(validators=[URLValidator()])
     promo_code = models.CharField(blank=True, default='', max_length=255)
     cost = models.DecimalField(max_digits=20, decimal_places=2)
+    is_active = models.BooleanField(default=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
