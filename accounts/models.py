aa9a416119924cf166428c817175807cb9f77fed
Darayus Nanavati <darayus.contact@gmail.com>
2013-09-20 12:34:15 +1000

    Added custom user profile model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    birthday = models.DateField(blank=True, null=True)
+    user = models.OneToOneField(User)
+
+
+def create_user_profile(sender, instance, created, **kwargs):
+    print sender, instance, created, kwargs
+    if created:
+        UserProfile.objects.create(user=instance)
+
+post_save.connect(create_user_profile, sender=User)
