4a2efa3b06d95983f52447c882209ba057663a99
Darayus Nanavati <darayus.contact@gmail.com>
2013-09-21 00:01:25 +1000

    Added provider link

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.db import models
+from offers.models import Provider
 
 
 class UserProfile(models.Model):
     birthday = models.DateField(blank=True, null=True)
     user = models.OneToOneField(User, related_name='user_profile')
+    provider = models.OneToOneField(Provider, blank=True, null=True, related_name="owners")
 
     def __unicode__(self):
         return "{0} profile".format(self.user.username)
