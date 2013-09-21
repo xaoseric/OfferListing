5ee50453b50129dc0ed2d0721e4775b931177d4c
Darayus Nanavati <darayus.contact@gmail.com>
2013-09-21 16:33:03 +1000

    Added fabric coverage tests

--- fabfile.py
+++ fabfile.py
@@ -6,13 +6,23 @@ def run_django_command(command):
     local('python manage.py {0}'.format(command))
 
 
+def set_test_environment():
+    os.environ["TEST_RUNNING"] = 'True'
+
+
 @task
 def test():
-    os.environ["TEST_RUNNING"] = 'True'
+    set_test_environment()
     run_django_command('test -v 2')
 
 
 @task
 def test_jenkins():
-    os.environ["TEST_RUNNING"] = 'True'
+    set_test_environment()
     run_django_command('jenkins -v 2 --traceback')
+
+
+@task
+def coverage():
+    set_test_environment()
+    local('coverage run manage.py test -v 2 && coverage html')
