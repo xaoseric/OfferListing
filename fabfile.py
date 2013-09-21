35d4832fd589370d0e51d36f38e1dc897ff40bbf
Darayus Nanavati <darayus.contact@gmail.com>
2013-09-21 16:29:51 +1000

    Updated tests for travis and jenkins

--- fabfile.py
+++ fabfile.py
@@ -6,6 +6,13 @@ def run_django_command(command):
     local('python manage.py {0}'.format(command))
 
 
+@task
 def test():
     os.environ["TEST_RUNNING"] = 'True'
-    run_django_command('test')
+    run_django_command('test -v 2')
+
+
+@task
+def test_jenkins():
+    os.environ["TEST_RUNNING"] = 'True'
+    run_django_command('jenkins -v 2 --traceback')
