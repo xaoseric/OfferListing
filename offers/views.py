0f7e3337e96f8315b780f85f93102bdf78612d3f
Darayus Nanavati <darayus.contact@gmail.com>
2013-09-23 19:55:14 +1000

    Added more logging

--- offers/views.py
+++ offers/views.py
@@ -99,6 +99,9 @@ def provider_profile(request, provider_pk, page_number=1):
         offers = paginator.page(page_number)
     except EmptyPage:
         # If page is out of range (e.g. 9999), deliver last page of results.
+        logger.info("Offer page got past range", exc_info=True, extra={
+            'request': request,
+        })
         offers = paginator.page(paginator.num_pages)
 
     return render(request, "offers/provider.html", {
@@ -118,6 +121,10 @@ def admin_provider_home(request):
         if form.is_valid():
             form.save()
             messages.success(request, "The provider's profile has been updated!")
+            logger.info("Provider was updated", exc_info=True, extra={
+                'request': request,
+                'provider': request.user.user_profile.provider.name,
+            })
             # Reload the form
             return HttpResponseRedirect(reverse('offer:admin_home'))
         else:
