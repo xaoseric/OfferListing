1427aa2bf21c8684cae6f80eb030863257518f65
Darayus Nanavati <darayus.contact@gmail.com>
2013-09-24 17:55:39 +1000

    Added more tags to the filters

--- template_helpers/cleaners.py
+++ template_helpers/cleaners.py
@@ -4,8 +4,8 @@ import bleach
 attrs = {
     '*': ['style']
 }
-tags = ['p', 'em', 'strong']
-styles = ['color', 'font-weight']
+tags = ['p', 'em', 'strong', 'ul', 'li', 's', 'h1', 'h2', 'h3', 'div', 'ol', 'hr', 'pre']
+styles = ['color', 'font-weight', 'font-style', 'color', 'background']
 
 
 def clean(text):
