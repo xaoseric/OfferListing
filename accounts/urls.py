faada6b4790aa29c14f0cfde9cbdddd0e9829853
Darayus Nanavati <darayus.contact@gmail.com>
2013-09-26 00:51:51 +1000

    Fixed other profile bugs

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ from accounts.forms import BetterAuthenticationForm
 urlpatterns = patterns('accounts.views',
     url(r'^register/$', 'register', name='register'),
     url(r'^profile/$', 'self_profile', name='self_profile'),
-    url(r'^(?P<username>[\w.@+-]+)/$', 'profile', name='profile'),
+    url(r'^user/(?P<username>[\w.@+-]+)/$', 'profile', name='profile'),
     url(r'^profile/comments/$', 'comment_list', name='my_comments'),
     url(r'^update/$', 'edit_account', name='edit_account'),
     url(r'^update/password/$', 'change_password', name='change_password'),
