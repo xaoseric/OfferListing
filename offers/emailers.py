9c0123dd3bcc59d7f12a5b653ad9a6acab6bb939
Darayus Nanavati <darayus.contact@gmail.com>
2013-09-27 16:39:02 +1000

    Added site url to email templates

--- offers/emailers.py
+++ offers/emailers.py
@@ -4,10 +4,28 @@ from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
 
 
-def send_simple_mail(subject, message, message_plain, to):
+def send_simple_mail(subject, message_template, message_plain_template, context, to):
 
-    msg = EmailMultiAlternatives(subject, message_plain, from_email=settings.DEFAULT_FROM_EMAIL, to=[to])
-    msg.attach_alternative(message, "text/html")
+    if settings.SITE_URL.endswith('/'):
+        site_url = 'http://' + settings.SITE_URL[:-1]
+    else:
+        site_url = 'http://' + settings.SITE_URL
+
+    context.update({"site_url": site_url})
+
+    message = render_to_string(message_template, context)
+    message_plain = render_to_string(message_plain_template, context)
+
+    msg = EmailMultiAlternatives(
+        subject,
+        message_plain,
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=[to]
+    )
+    msg.attach_alternative(
+        message,
+        "text/html"
+    )
     msg.send()
 
 
@@ -15,7 +33,10 @@ def send_comment_reply(comment):
     if not comment.is_reply():
         return
 
-    message = render_to_string('offers/email/comment_reply.html', {"comment": comment})
-    message_plain = render_to_string('offers/email/comment_reply_plain.txt', {"comment": comment})
-
-    send_simple_mail('New reply to your comment', message, message_plain, comment.reply_to.commenter.email)
+    send_simple_mail(
+        subject='New reply to your comment',
+        message_template='offers/email/comment_reply.html',
+        message_plain_template='offers/email/comment_reply_plain.txt',
+        context={"comment": comment},
+        to=comment.reply_to.commenter.email
+    )
