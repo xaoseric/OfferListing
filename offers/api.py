e480064631480480abfa5afc79390a10c5881e29
Darayus Nanavati <darayus.contact@gmail.com>
2013-09-28 02:55:12 +1000

    Allow some filtering

--- offers/api.py
+++ offers/api.py
@@ -1,6 +1,7 @@
 from tastypie.resources import ModelResource
 from tastypie import fields
 from offers.models import Plan, Offer, Location
+from tastypie.constants import ALL, ALL_WITH_RELATIONS
 
 
 class OfferResource(ModelResource):
@@ -8,12 +9,22 @@ class OfferResource(ModelResource):
     class Meta:
         queryset = Offer.objects.filter(status=Offer.PUBLISHED)
         resource_name = 'offer'
+        allowed_methods = ['get', 'post']
+
 
 
 class LocationResource(ModelResource):
 
     class Meta:
         queryset = Location.objects.all()
+        resource_name = 'location'
+        allowed_methods = ['get', 'post']
+        filtering = {
+            "country": ALL,
+            "city": ALL,
+            "datacenter": ALL,
+        }
+
 
 
 class PlanResource(ModelResource):
@@ -24,3 +35,7 @@ class PlanResource(ModelResource):
     class Meta:
         queryset = Plan.objects.filter(is_active=True, offer__status=Offer.PUBLISHED, offer__is_active=True)
         resource_name = 'plan'
+        filtering = {
+            "bandwidth": ALL,
+            "location": ALL_WITH_RELATIONS,
+        }
