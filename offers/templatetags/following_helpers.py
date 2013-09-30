dc77dec34270484d7579e909735a83804f72dd4c
Darayus Nanavati <darayus.contact@gmail.com>
2013-09-30 21:31:39 +1000

    Added badge to total followers

--- offers/templatetags/following_helpers.py
+++ offers/templatetags/following_helpers.py
@@ -9,7 +9,13 @@ def following(request, offer):
     if not request.user.is_authenticated():
         return ""
 
-    if not offer.followers.filter(pk=request.user.pk).exists():
-        return '<a class="btn btn-success" href="%s">Follow</a>' % (offer.get_absolute_url() + '?do=follow')
+    total_followers = offer.followers.count()
 
-    return '<a class="btn btn-warning" href="%s">Unfollow</a>' % (offer.get_absolute_url() + '?do=unfollow')
+    badge = ""
+    if total_followers:
+        badge = ' <span class="badge">{0}</span>'.format(total_followers)
+
+    if not offer.followers.filter(pk=request.user.pk).exists():
+        return '<a class="btn btn-success" href="%s">Follow%s</a>' % (offer.get_absolute_url() + '?do=follow', badge)
+
+    return '<a class="btn btn-warning" href="%s">Unfollow%s</a>' % (offer.get_absolute_url() + '?do=unfollow', badge)
