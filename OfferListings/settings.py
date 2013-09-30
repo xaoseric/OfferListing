a6a2f760a10829485c7500bef49169323ad9478a
Darayus Nanavati <darayus.contact@gmail.com>
2013-10-01 02:52:09 +1000

    Added redis as a results backend

--- OfferListings/settings.py
+++ OfferListings/settings.py
@@ -28,6 +28,7 @@ DATABASES = {
 
 djcelery.setup_loader()
 BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 CELERYBEAT_SCHEDULER = "djcelery.schedulers.DatabaseScheduler"
 
 CELERYBEAT_SCHEDULE = {
