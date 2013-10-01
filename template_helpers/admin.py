04b62112c252e42eb86896d8d66af6eef65b18da
Darayus Nanavati <darayus.contact@gmail.com>
2013-10-01 18:58:37 +1000

    Made advanced options not options

---
+++ template_helpers/admin.py
@@ -0,0 +1,25 @@
+from django.contrib.flatpages.admin import FlatPageAdmin, FlatpageForm
+from django.contrib import admin
+from django.contrib.flatpages.models import FlatPage
+from django.db import models
+from django import forms
+
+
+class BetterFlatPageAdmin(FlatPageAdmin):
+
+    #formfield_overrides = {
+    #    models.TextField: {'widget': forms.Textarea(attrs={'class': 'ckeditor'})},
+    #}
+
+    fieldsets = (
+        (None, {
+            'fields': ('url', 'title', 'content', 'sites')
+        }),
+    )
+
+    class Media:
+        js = ('ckeditor/ckeditor.js',)
+
+
+admin.site.unregister(FlatPage)
+admin.site.register(FlatPage, BetterFlatPageAdmin)
