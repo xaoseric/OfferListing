c8af6d84b9089d5eabb65636f7a008889f19aaed
Darayus Nanavati <darayus.contact@gmail.com>
2013-10-03 13:52:23 +1000

    Added history for offers

--- OfferListings/settings.py
+++ OfferListings/settings.py
@@ -117,6 +117,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
+    'reversion.middleware.RevisionMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
