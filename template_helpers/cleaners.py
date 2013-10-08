b7cb7e8741ec158d4c0c5084073bab45c1bf174d
Darayus Nanavati <darayus.contact@gmail.com>
2013-10-08 23:14:30 +1100

    Allow links for offers

--- template_helpers/cleaners.py
+++ template_helpers/cleaners.py
@@ -3,9 +3,9 @@ import bleach
 
 def clean(text):
     attrs = {
-        '*': ['style']
+        '*': ['style', 'href']
     }
-    tags = ['p', 'em', 'strong', 'ul', 'li', 's', 'h1', 'h2', 'h3', 'div', 'ol', 'pre', 'blockquote']
+    tags = ['p', 'em', 'strong', 'ul', 'li', 's', 'h1', 'h2', 'h3', 'div', 'ol', 'pre', 'blockquote', 'a']
     styles = ['color', 'font-weight', 'font-style', 'color', 'background', 'border', 'padding']
     return bleach.clean(text, tags, attrs, styles, strip=True)
 
