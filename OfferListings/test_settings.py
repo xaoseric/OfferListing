d54aca6b771d4dbc8cc7638ad885f6318fe3b28e
Novacha Man <novachalon@gmail.com>
2013-10-19 17:44:05 +1100

    Made travis tests use correct db settings

--- OfferListings/test_settings.py
+++ OfferListings/test_settings.py
@@ -1,2 +1,21 @@
+import os
+
 CELERY_ALWAYS_EAGER = True
 CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
+
+DATABASE_TYPE = os.getenv('TEST_DB', 'mysql')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'offer_listings',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+if DATABASE_TYPE == 'sqlite':
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
+    DATABASES['default']['NAME'] = 'database.sqlite'
