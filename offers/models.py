4577efe1814ec822d7bce87e7036d85ed818accb
Novacha Man <novachalon@gmail.com>
2013-10-20 19:17:03 +1100

    Updated ordering meta

--- offers/models.py
+++ offers/models.py
@@ -678,3 +678,6 @@ class Review(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ['-created_at']
+
