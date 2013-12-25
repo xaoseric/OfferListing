8a64a27be19b5698c0b90000b23c50abb3bd660a
Novacha Man <novachalon@gmail.com>
2013-12-25 20:20:15 +1100

    Changed development back to sqlite

--- OfferListings/settings.py
+++ OfferListings/settings.py
@@ -16,8 +16,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'offer_listings',   # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(BASE_PATH, 'database.sqlite'),   # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'root',
         'PASSWORD': '',
