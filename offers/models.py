82351ad5bb6ea56694f105a906349e2c671eb58b
Novacha Man <novachalon@gmail.com>
2013-12-25 12:52:51 +1100

    Fixed get_plan_locations for new ManyToMany

--- offers/models.py
+++ offers/models.py
@@ -326,8 +326,9 @@ class Offer(models.Model):
     def get_plan_locations(self):
         locations = []
         for plan in self.plan_set.all():
-            if plan.location not in locations:
-                locations.append(plan.location)
+            for location in plan.locations:
+                if plan.location not in locations:
+                    locations.append(plan.location)
         return locations
 
     def get_min_max_cost(self):
