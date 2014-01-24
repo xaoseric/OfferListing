39202fff8ad5aae10d8678c5f5f0ae3a47a7d93d
Novacha Man <novachalon@gmail.com>
2014-01-24 11:37:17 +1100

    Move to whoosh

--- OfferListings/settings.py
+++ OfferListings/settings.py
@@ -197,9 +197,8 @@ INSTALLED_APPS = (
 # Haystack settings
 HAYSTACK_CONNECTIONS = {
     'default': {
-        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
-        'URL': 'http://127.0.0.1:9200/',
-        'INDEX_NAME': 'offerlistings',
+        'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
+        'PATH': os.path.join(BASE_PATH, 'offers_index'),
     },
 }
 
