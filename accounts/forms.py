1fe1f808055c35ad0b626245eafb6446a585b1ff
Eric Hocking <erich@xaos-ia.com>
2014-04-26 12:15:36 -0400

    Add password reset form

--- accounts/forms.py
+++ accounts/forms.py
@@ -23,6 +23,15 @@ class UserEditForm(forms.Form):
         self.helper = FormHelper()
         self.helper.add_input(Submit('update', 'Update Account'))
 
+class UserPassResetForm(forms.Form):
+    username = forms.CharField(max_length=30)
+    email = forms.EmailField(max_length=75)
+
+    def __init__(self, *args, **kwargs):
+        super(UserPassResetForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.add_input(Submit('initpwreset', 'Get Password Reset Link'))
+
 
 class UserConfirmDeletionForm(forms.Form):
 
